c927c3d617b816f9a9d350dbcfda482de1c8a2c6
rpk0 <karampinas.k@gmail.com>
2013-04-05 15:44:57 +0300

    more tests

--- spec/dissect_spec.rb
+++ spec/dissect_spec.rb
@@ -30,12 +30,16 @@ describe Dissect do
     mailenc = Mail.read File.expand_path("spec/test_files/encoded.eml")
     # multipart - xml
     mailxml  = Mail.read File.expand_path("spec/test_files/xml.eml")
+    @plain_text = "phone number: 111-123-4567"
     input_type1 = "email"
     input_type2 = "xml"
+    input_type3 = "text"
     arr = ["4", "11", "31", "14"]
     @str1 = Dissect::to_plaintext(mailhtml, input_type1)
     @str2 = Dissect::to_plaintext(mailxml, input_type2)
     @str3 = Dissect::to_plaintext(mailxml, input_type1)
+    @str4 = Dissect::to_plaintext(mailtext, input_type1)
+    @str5 = Dissect::to_plaintext(@plain_text, input_type3)
     @test_reg1 = Dissect::to_regexp("/^\D*\d\D*\d/i")
     @test_reg2 = Dissect::to_regexp("/^\D*\d\D*\d/x")
     @test_reg3 = Dissect::to_regexp("/^\D*\d\D*\d/m")
@@ -61,6 +65,15 @@ describe Dissect do
     it "should raise" do
       expect{Dissect::to_plaintext(mailenc, input_type1)}.to raise_error
     end
+
+    it "should just return the body devoded" do
+      @str4 == "\nDear Mr. User,\n\ntest body! just for @testing\n----"
+    end
+
+    it "should return the same string" do
+      @str5 == @plain_text
+    end
+
   end
 
   describe '#to_regexp' do
@@ -99,35 +112,41 @@ describe Dissect do
 
   before :each do
     root = Dissect::root
-    @path = File.join(root, "/config/dissect")
+    @path = Dissect::set_config_paths
+    @dir = File.join(root, @path)
   end
 
-  describe '#process' do
-    it "must create the config directory" do
-      Pathname(@path).should exist
+  describe '#set_config_paths' do
+    it "should eq the right path" do
+      @path == "/config/dissect"
     end
   end
 
-  describe '#process' do
-    specify 'works when I do stub' do
-      Dissect.should_receive(:process)
-      Dissect.process("a",["2"],"b","c")
+  describe '#set_config_dir' do
+    it "must create the config directory" do
+      Pathname(@dir).should exist
     end
   end
 
 # ##############################################################################
 
   before :each do
-    @regexes = YAML.load_file(File.expand_path("spec/test_files/test.yml"))["non_fixed_structure"]["regexes"]
-    str     = "phone number: 111-123-4567"
-    # options = YAML.load_file(File.expand_path("spec/test_files/test.yml"))["fixed_structure"]["options"]
-    # structure = YAML.load_file(File.expand_path("spec/test_files/test.yml"))["fixed_structure"]["options"]["structure"]
-    # str2    = ""
+    yml = YAML.load_file(File.expand_path("spec/test_files/test.yml"))
+    regexes = yml["non_fixed_structure"]["regexes"]
+    @str     = "phone number: 111-123-4567"
+    @str2    = "startt  1  AA00000     Superwow Item1 - version4          EUR 113.34
+          ???O 113.34\n                 Edition\n  1  AA000000000 Superwow Item2
+                           EUR 41.01       ???O 41.01\n     11           endd
+            phone number: 111-123-4567"
+    options = yml["fixed_structure"]["options"]
+    @structure = options["structure"]
 
-    @output = Dissect.unstructured_parser(@regexes, str)
-    # @output2 = Dissect.fixed_width_parser(options, structure, str2)
+    @output = Dissect.unstructured_parser(regexes, @str)
+    @output2 = Dissect.fixed_width_parser(options, @structure, @str2)
     @result_json  = Dissect.result(@output, "json")
-    # @result_xml   = Dissect.result(@output, "xml")
+    @result_json2  = Dissect.result(@output2, "json")
+    @result_xml   = Dissect.result(@output, "xml")
+
   end
 
   describe '#unstructured_parser' do
@@ -141,25 +160,72 @@ describe Dissect do
     end
   end
 
-  # describe '#fixed_width_parser' do
-  #   it "should return a hash with matches" do
-  #     @output2.should be_an_instance_of(Hash)
-  #   end
-  # end
+  describe '#fixed_width_parser' do
+    it "should return a hash with matches" do
+      @output2.should be_an_instance_of(Hash)
+    end
+  end
 
   # it prints things -fix it
   describe '#result' do
     it "should be a correct json" do
       @result_json == @output.to_json
+      @result_json2 == @output2.to_json
     end
-    # it "should be a  correct xml" do
-    #   @result_xml.should have_tag("<content>")
-    # end
+    it "should be a  correct xml" do
+      @result_xml.should match(/\<\?xml version/)
+      # @result_xml.should have_selector("<?xml version=\"1.0\"?>")
+    end
+  end
+
+# ##############################################################################
+
+  before(:each) do
+    Dissect.stub(:set_config_paths).and_return("spec/test_files/")
+    Dissect.stub(:root).and_return(File.expand_path '../..', __FILE__)
+    @load = Dissect.regex_loader("test")
+    @valid_i = Dissect.valid_identifier
+
+
+    @final1 = Dissect.process(@str, ["test_un"], "text", "json")
+    @final2 = Dissect.process(@str2, ["test"], "text", "json")
   end
 
   describe '#process' do
     it "should raise ArgumentError" do
       expect{ Dissect.process.validate_arguments(nil) }.to raise_error(ArgumentError)
+    end
+
+    it "should raise ArgumentError" do
+      expect{ Dissect.process.(nil, "kati") }.to raise_error
+    end
+
+    it "should raise ArgumentError" do
+      expect{ Dissect.process.("kati", nil) }.to raise_error
+    end
+
+    # it "should have correct arguments" do
+    #   Dissect.process.should_receive(:process).with(kind_of(String), kind_of(Array))
+    # end
+  end
+
+  describe '#regex_loader' do
+    it "should load yml contents to a hash" do
+      @load.should be_an_instance_of(Hash)
+    end
+  end
+
+  describe '#valid_identifier' do
+    it "should return an array of available config files" do
+      @valid_i.should be_an_instance_of(Array)
+      @valid_i == ["test","test_un"]
+    end
+  end
+
+  describe '#process' do
+    it "should return the output hash to a json" do
+      @final1 == @output
+      @final2 == @output2
     end
   end
 
@@ -171,21 +237,5 @@ describe Dissect do
   #   end
   #   # Dissect.process("a",["2"],"b","c")
   # end
-  # describe '#process' do
-  #   it "should raise ArgumentError for arguments that are not boolean" do
-  #     expect{ Foo.validate_arguments(nil) }.to raise_error(ArgumentError)
-  # end
-
-  # before :each do
-  #   a="1", b="2", c="3", d="4", e="5", f="6"
-  #   @res = Dissect::result(b,c,d,e,f).values.any? &:empty?
-  # end
-
-  # describe '#process' do
-  #   it "returns no empty output values" do
-  #     @res.should_not be_true
-  #   end
-  # end
 
 end
-
